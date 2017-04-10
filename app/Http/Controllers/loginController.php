8d5db7b6235285b77139446d6d62f6ab0b1e363d
TimothyMwaisaka <cultimmy@gmail.com>
2017-04-10 09:15:52 +0300

    Added login function and routes access control

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -1,13 +1,29 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Auth;
+use App\User;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
-abstract class loginController extends BaseController
+class loginController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function login(Request $request){
+        if (Auth::attempt([
+            'username' => $request->username,
+            'password' => $request->password
+        ])){
+            $user = User::where('username', $request->username)->first();
+            if($user->is_admin()){
+                return redirect()->route('dashboard');
+            }
+            return redirect()->route('student');
+        }
+        return redirect()->route('admin');
+    }
 }
